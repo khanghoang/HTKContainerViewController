b24e2ad418c9b448dcb3dbc3ef7aa022cea9e657
Triệu Khang <hoangtrieukhang@gmail.com>
2013-10-14 17:31:14 +0700

    Initial Commit

---
+++ UIContainerViewController/HTKViewController.m
@@ -0,0 +1,29 @@
+//
+//  HTKViewController.m
+//  UIContainerViewController
+//
+//  Created by Triệu Khang on 14/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import "HTKViewController.h"
+
+@interface HTKViewController ()
+
+@end
+
+@implementation HTKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
